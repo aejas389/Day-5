e23655bbb70cad331630aa6767264368f4010233
aejas389 <23hr1a3252@mtieat.org>
2024-12-06 15:11:43 +0530

    Add files via upload

---
+++ nestecloop.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class nestecloop
+ {
+    public static void main(String[] args) {
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;i<=n;j++)
+        }
+        System.out.println(n);
+    }
+    {
+        System.out.println();
+    }
+    
+}
